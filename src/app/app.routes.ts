1eed182189dbb3c36424fcf83172068875bf6cc5
Nacktaker <hunzolthx@gmail.com>
2025-12-15 17:11:22 +0700

    oo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import { HelloWorld } from './hello-world/hello-world';
+import { Profile } from './profile/profile';
+import { Friend } from './friend/friend';
+import { Status } from './status/status';
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'hello-world',
+    pathMatch: 'full',
+  }, // default route
+  {
+    path: '',
+    redirectTo: 'profile',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: 'friend',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: 'status',
+    pathMatch: 'full',
+  },
+  { path: 'hello-world', component: HelloWorld },
+  { path: 'profile', component: Profile },
+  { path: 'friend', component: Friend },
+  { path: 'status', component: Status },
+];
